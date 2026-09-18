437c98ba50d3b48fe9384de29341b3d71f503c85
parkjieun <parkjieun3052@gmail.com>
2026-09-18 15:02:09 +0900

    메뉴페이지

--- menu.js
+++ menu.js
@@ -1,8 +1,11 @@
-//메뉴 버튼 
 const menuButton = document.getElementById("menuButton");
 const menuWindow = document.getElementById("menuWindow");
 const menuClose = document.getElementById("menuClose");
 
-menuButton.addEventListener("click", () => {menuWindow.classList.add("active");});
+menuButton.onclick = function () {
+    menuWindow.classList.add("active");
+};
 
-menuClose.addEventListener("click", () => {menuWindow.classList.remove("active");});
+menuClose.onclick = function () {
+    menuWindow.classList.remove("active");
+};
