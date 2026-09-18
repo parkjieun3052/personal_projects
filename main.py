f32ac3a4e15b4c500fc589c225b0f95d3c386199
parkjieun <parkjieun3052@gmail.com>
2026-09-18 11:42:09 +0900

    서버연결하기

---
+++ main.py
@@ -0,0 +1,25 @@
+import http.server
+
+print(http.server)
+base_handler = http.server.BaseHTTPRequestHandler
+simple_handler = http.server.SimpleHTTPRequestHandler
+http_server = http.server.HTTPServer
+
+
+class MyServer(simple_handler):
+    def do_GET(self): 
+        if self.path == "/":
+            with open("index.html", "r", encoding="utf-8") as file: page = file.read()
+
+            self.send_response(200)
+            self.send_header("Content_Type", "text/html; charset=utf-8")
+            self.end_headers()
+
+            self.wfile.write(page.encode("utf-8"))
+
+        else:
+            super().do_GET()
+
+server = http_server(("127.0.0.1",1234), MyServer)
+print("서버 가동중입니다.")
+server.serve_forever()
